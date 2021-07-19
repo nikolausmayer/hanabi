13e628fbbad7fd54d0134ec038d1760e9e0a0a96
Nikolaus Mayer <mayern@cs.uni-freiburg.de>
2021-07-19 21:47:07 +0200

    first actually working version

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
+#include <array>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -37,12 +38,13 @@
 #include <agg_path_storage.h>
 
 #include <FL/Fl.H>
+#include <FL/Fl_Box.H>
 #include <FL/Fl_Image.H>
 #include <FL/Fl_PNG_Image.H>
 #include <FL/Fl_Shared_Image.H>
 #include <FL/Fl_Value_Slider.H>
+#include <FL/Fl_Group.H>
 #include <FL/Fl_Window.H>
-#include <FL/Fl_Box.H>
 
 constexpr int W{512};
 constexpr int H{512};
@@ -52,20 +54,25 @@ struct Layer {
   float diameter;
   float spacing;
   float angle;
+  bool dirty;
+
+  unsigned char* alphamask_buf;
+  agg::path_storage paths;
 };
 Layer layers[] = {
-  {{200, 200, 255},  1.000, 6, 0},
-  {{255, 255,   0},  1.375, 6, 0},
-  {{255,   0,   0},  1.750, 6, 0},
-  {{  0,   0, 255},  2.125, 6, 0},
-  {{  0,   0,   0},  2.500, 6, 0},
+  {{200, 200, 255},  1.000, 6, 0, true, new unsigned char[W * H]},
+  {{255, 255,   0},  1.375, 6, 0, true, new unsigned char[W * H]},
+  {{255,   0,   0},  1.750, 6, 0, true, new unsigned char[W * H]},
+  {{  0,   0, 255},  2.125, 6, 0, true, new unsigned char[W * H]},
+  {{  0,   0,   0},  2.500, 6, 0, true, new unsigned char[W * H]},
 };
 
 unsigned char* imgdata{nullptr};
 Fl_RGB_Image* img{nullptr};
 Fl_Window* win{nullptr};
 Fl_Box* imgbox{nullptr};
-Fl_Value_Slider* slider{nullptr};
+Fl_Box* labels[5] = {nullptr};
+Fl_Value_Slider* sliders[5] = {nullptr};
 
 unsigned char* canvas_buf{nullptr};
 
@@ -79,7 +86,6 @@ void Render()
   // Alpha masks
   static unsigned char* alphamask_buf_outside{nullptr};
   static unsigned char* alphamask_buf_ring{nullptr};
-  static unsigned char* alphamask_buf = new unsigned char[W * H];
   // Outer area
   if (not alphamask_buf_outside) {
     alphamask_buf_outside = new unsigned char[W * H];
@@ -138,12 +144,10 @@ void Render()
     ren_base.clear(agg::rgba8(255, 255, 255));
   }
 
-  for (const auto& layer : layers) {
-
+  for (auto& layer : layers) {
     // Hexagonal pattern
-    {
-      rbuf_alpha.attach(alphamask_buf, W, H, W);
-
+    rbuf_alpha.attach(layer.alphamask_buf, W, H, W);
+    if (layer.dirty) {
       agg::pixfmt_gray8 pixf(rbuf_alpha);
       agg::renderer_base ren_base(pixf);
       agg::renderer_scanline_aa_solid renderer(ren_base);
@@ -151,39 +155,40 @@ void Render()
 
       ren_base.clear(agg::gray8(255));
 
-      agg::path_storage paths;
-      int yi = 0;
-      int xi = 0;
-      int direction = 0;
-      int streaksize = 1;
-      int streak = 0;
-      int switches = 0;
-      while (true) {
-        float xc = xi * layer.spacing + (yi % 2 == 0 ? 0 : 0.5 * layer.spacing);
-        float yc = yi * 1.732/2 * layer.spacing;
-        if (std::pow(xc, 2) + std::pow(yc, 2) < 200 * 200) {
-          agg::ellipse ell;
-          ell.init(W/2 + xc, H/2 + yc, layer.diameter, layer.diameter, 32);
-          paths.concat_path(ell);
-        }
+      if (layer.paths.total_vertices() == 0) {
+        int yi = 0;
+        int xi = 0;
+        int direction = 0;
+        int streaksize = 1;
+        int streak = 0;
+        int switches = 0;
+        while (true) {
+          float xc = xi * layer.spacing + (yi % 2 == 0 ? 0 : 0.5 * layer.spacing);
+          float yc = yi * 1.732/2 * layer.spacing;
+          if (std::pow(xc, 2) + std::pow(yc, 2) < 200 * 200) {
+            agg::ellipse ell;
+            ell.init(W/2 + xc, H/2 + yc, layer.diameter, layer.diameter, 32);
+            layer.paths.concat_path(ell);
+          }
 
-        if (W/2 + xc < 0 or W/2 + xc >= W or
-            H/2 + yc < 0 or H/2 + yc >= H)
-          break;
+          if (W/2 + xc < 0 or W/2 + xc >= W or
+              H/2 + yc < 0 or H/2 + yc >= H)
+            break;
 
-        switch (direction) {
-          case 0: {--yi; break;}
-          case 1: {++xi; break;}
-          case 2: {++yi; break;}
-          case 3: {--xi; break;}
-        }
-        ++streak;
-        if (streak == streaksize) {
-          direction = (direction + 1) % 4;
-          streak = 0;
-          ++switches;
-          if (switches % 2 == 0)
-            ++streaksize;
+          switch (direction) {
+            case 0: {--yi; break;}
+            case 1: {++xi; break;}
+            case 2: {++yi; break;}
+            case 3: {--xi; break;}
+          }
+          ++streak;
+          if (streak == streaksize) {
+            direction = (direction + 1) % 4;
+            streak = 0;
+            ++switches;
+            if (switches % 2 == 0)
+              ++streaksize;
+          }
         }
       }
 
@@ -191,7 +196,7 @@ void Render()
       mtx *= agg::trans_affine_translation(-W/2, -H/2);
       mtx *= agg::trans_affine_rotation(layer.angle);
       mtx *= agg::trans_affine_translation(W/2, H/2);
-      agg::conv_transform<agg::path_storage, agg::trans_affine> trans(paths, mtx);
+      agg::conv_transform trans(layer.paths, mtx);
       rasterizer.add_path(trans);
 
       renderer.color(agg::gray8(0));
@@ -199,9 +204,11 @@ void Render()
 
       for (std::size_t i = 0; i < W*H; ++i) {
         /// Add outer ring
-        alphamask_buf[i] = std::max(alphamask_buf[i], alphamask_buf_ring[i]);
+        layer.alphamask_buf[i] = std::max(layer.alphamask_buf[i], 
+                                          alphamask_buf_ring[i]);
         /// Blank out outer area
-        alphamask_buf[i] = std::min(alphamask_buf[i], alphamask_buf_outside[i]);
+        layer.alphamask_buf[i] = std::min(layer.alphamask_buf[i], 
+                                          alphamask_buf_outside[i]);
       }
     }
 
@@ -222,14 +229,20 @@ void Render()
       agg::render_scanlines(rasterizer, sl, renderer);
     }
 
+    layer.dirty = false;
   }  /// <- Layers loop
 }
 
 
 void Update(Fl_Widget *w)
 {
-  const float angle = slider->value();
-  layers[3].angle = angle * M_PI / 180;
+  for (std::size_t i = 0; i < std::size(sliders); ++i) {
+    if (const float angle = sliders[i]->value() * M_PI / 180; 
+        layers[i].angle != angle) {
+      layers[i].angle = angle;
+      layers[i].dirty = true;
+    }
+  }
 
   Render();
 
@@ -266,12 +279,25 @@ int main()
   imgbox->image(img);
   imgbox->show();
 
-  slider = new Fl_Value_Slider(W,0,400,25);
-  slider->type(FL_HOR_NICE_SLIDER);
-  slider->bounds(-60, 60);
-  slider->value(0);
-  slider->callback(Update);
-  slider->show();
+  for (std::size_t i = 0; i < std::size(sliders); ++i) {
+    constexpr std::array colornames{"lightblue", "yellow", "red", "blue", "black"};
+    Fl_Box* label = new Fl_Box(W, i*25, 70, 25, colornames[i]);
+    //label->box(FL_UP_BOX);
+    //label->labelsize(36);
+    //label->labelfont(FL_BOLD+FL_ITALIC);
+    //label->labeltype(FL_SHADOW_LABEL);
+    constexpr std::array colors{0x9999ff00, FL_YELLOW, FL_RED, FL_BLUE, FL_BLACK};
+    label->labelcolor(colors[i]);
+    labels[i] = label;
+
+    Fl_Value_Slider* slider = new Fl_Value_Slider(W+70, i*25, 330, 25);
+    slider->type(FL_HOR_NICE_SLIDER);
+    slider->bounds(-60, 60);
+    slider->value(0);
+    slider->callback(Update);
+    slider->show();
+    sliders[i] = slider;
+  }
 
   win->end();
   win->show();
